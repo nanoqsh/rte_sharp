c846cfd2708c86932719d0b1248a76fdbb5838ed
nekosora <thenekosora@gmail.com>
2019-03-02 17:49:57 +0600

    add indexer

---
+++ RTE/Engine/Indexer.cs
@@ -0,0 +1,25 @@
+using RTE.OBJReader.OBJFormat;
+using System.Collections.Generic;
+
+namespace OBJReader
+{
+    public static class Indexer
+    {
+        public static IEnumerable<int> GetIndexes(IFaceUnit[] faces, int start = 0)
+        {
+            Dictionary<IFaceUnit, int> units = new Dictionary<IFaceUnit, int>();
+            int index = start;
+
+            foreach (IFaceUnit unit in faces)
+                if (!units.ContainsKey(unit))
+                    units.Add(unit, index++);
+            
+            int[] indexes = new int[faces.Length];
+
+            for (int i = 0; i < indexes.Length; i++)
+                indexes[i] = units[faces[i]];
+
+            return indexes;
+        }
+    }
+}
